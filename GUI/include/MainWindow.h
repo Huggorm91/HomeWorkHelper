f1ad0503725ee1b68b2b960960b7d849379e6816
Huggorm91 <olaus@klaveness.dev>
2025-09-30 19:13:43 +0200

    Create project

---
+++ GUI/include/MainWindow.h
@@ -0,0 +1,15 @@
+//
+// Created by olaus on 2025-09-28.
+//
+
+#ifndef HOMEWORKHELPER_MAINWINDOW_H
+#define HOMEWORKHELPER_MAINWINDOW_H
+
+#include <gtkmm-4.0/gtkmm/window.h>
+#include <gtkmm-4.0/gtkmm/button.h>
+
+class MainWindow {
+};
+
+
+#endif //HOMEWORKHELPER_MAINWINDOW_H
---
+++ GUI/source/MainWindow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by olaus on 2025-09-28.
+//
+
+#include "MainWindow.h"
