314dd8c445c5df6347778663d1544ae9c596b813
Huggorm91 <olaus@klaveness.dev>
2025-12-27 23:21:58 +0100

    Fixed infinite loop issue with Divide class

--- Common/Math/Solvers/Divide.cpp
+++ Common/Math/Solvers/Divide.cpp
@@ -34,13 +34,23 @@ namespace Math
     {
         SolverBase::GenerateNumbers();
         if (myIsUsingFloats) {
+            int counter = 0;
             while(mySecondNumber.f == 0.f) {
                 mySecondNumber.f = myNumberGenerator.f(myEngine);
+                ++counter;
+                if (counter == 10) {
+                    mySecondNumber.f = 1.f;
+                }
             }
         }
         else {
+            int counter = 0;
             while(mySecondNumber.i == 0) {
                 mySecondNumber.i = myNumberGenerator.i(myEngine);
+                ++counter;
+                if (counter == 10) {
+                    mySecondNumber.i = 1;
+                }
             }
         }
     }
